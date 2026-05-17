48910488b3dbb8198e7253cbe60940b06fba986e
henrique Nandi <henrique-nandi@hotmail.com>
2026-05-17 08:16:10 +0100

    Fix: mount Sonner Toaster so sale errors and success messages are visible

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { Toaster } from "@/components/ui/toaster"
+import { Toaster as SonnerToaster } from "@/components/ui/sonner"
 import { QueryClientProvider } from '@tanstack/react-query'
 import { queryClientInstance } from '@/lib/query-client'
 import NavigationTracker from '@/lib/NavigationTracker'
@@ -79,6 +80,7 @@ function App() {
           <AuthenticatedApp />
         </Router>
         <Toaster />
+        <SonnerToaster richColors position="top-center" />
       </QueryClientProvider>
     </AuthProvider>
   )
